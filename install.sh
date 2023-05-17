bc21e58aaf622fc0ae4ce7fc6bf6c4892381c8f5
anthidfk420 <73663954+anthidfk420@users.noreply.github.com>
2023-05-17 12:45:42 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# TARDIS (Taz's Automatic Ricing & Desktop Installation Script)
+# I'm sooooo good at acronyms as you can tell
+# Contact: taz@notnapoleon.net (EMAIL) or @notnapoleon:envs.net (MATRIX)
+
+# FUNCTIONS
+
+aur_install() {
+	pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ~ && rm -rf yay
+}
+
+add_repos() {
+	echo "[multilib]" >> /etc/pacman.conf 
+	echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+}
+
+pacman_packages() {
+	pacman -S i3 polybar librewolf thunar nitrogen neofetch htop gthumb alacritty zsh mpv gnu-free-fonts vim leafpad noto-fonts noto-fonts-cjk gvfs-smb gnome-screenshot curl wget sddm pavucontrol pop-gtk-theme arc-icon-theme cups thunderbird 
+}
+
+aur_packages() {
+	yay -S hollywood peazip-gtk2-bin cava
+}
+
+systemd_services() {
+	systemctl enable sddm.service
+	systemctl enable cups.service
+}
+
+dotfiles_configure() {
+	cp -r ~/tardis/dotfiles/* ~/.config
+}
